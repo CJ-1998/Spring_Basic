fa8e2eeb7d7c4f2b82ac478ef9c97720c00ba687
CJ <choijun0627@konkuk.ac.kr>
2023-07-06 03:01:41 +0900

    <feat> 스프링 핵심 원리 기본편 프로젝트 section 3
    스프링으로 변환 위한 AppConfig 변경
    
    스프링 사용하기 위해 AppConfig 변경
    스프링 사용하는 것으로 MemberApp, OrderApp 클래스 변경

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,7 +1,6 @@
 package hello.core;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
@@ -9,22 +8,29 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
+
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
-//        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
     }
 }
