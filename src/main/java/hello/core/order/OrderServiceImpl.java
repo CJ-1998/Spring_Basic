cbfac089e8240e86dba4567dea098223b315d72f
CJ <choijun0627@konkuk.ac.kr>
2023-07-05 14:51:14 +0900

    <feat> 스프링 핵심 원리 기본편 프로젝트 section 3
    DIP 원칙 지키기 위한 AppConfig 생성
    
    구현체 객체들을 생성해서 의존관계 주입해주는 Appconfig 생성
    실제 클래스에서 구현체 의존하지 않고 생성자로 주입 받기 위해
    MemberServiceImpl, OrderServiceImpl 생성자 생성
    생성자 생성된 것으로 test하기 위해 MemberApp, OrderApp,
    MemberServiceTest, OrderServiceTest 수정

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -8,10 +8,16 @@ import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
 //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
 //    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
-    private DiscountPolicy discountPolicy;
+
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
