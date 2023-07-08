2a1108c230e75ca741f17f4f00287620c0fe05af
CJ <choijun0627@konkuk.ac.kr>
2023-07-08 15:23:52 +0900

    <test> 스프링 핵심 원리 기본편 프로젝트 section 5
    @Configuration의 효과 알아보기 위한 테스트 진행
    
    AppConfig 스프링 빈 출력했을 때 다른 AppConfig임을 확인하기 위한 테스트
    추가

--- src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -28,4 +28,14 @@ public class ConfigurationSingletonTest {
 
         assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
     }
+
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        //AppConfig도 스프링 빈으로 등록된다.
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean = " + bean.getClass());
+        //출력: bean = class hello.core.AppConfig$$EnhancerBySpringCGLIB$$bd479d70
+    }
 }
