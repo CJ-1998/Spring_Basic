196a3c9586e52eec716f8747c97a8cc3ca2acbc9
CJ <choijun0627@konkuk.ac.kr>
2023-07-10 00:16:04 +0900

    <refactor> 스프링 핵심 원리 기본편 프로젝트 section 7
    @Qualifier 사용 시 편리하기 위해 annotation 정의
    
    @Qualifier 사용할 때 더 깔끔하고 편하게 사용하기 위해
    MainDiscountPolicy annotation 정의
    annotation 사용하기 위해 @Qualifier 사용하던 곳 수정

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
@@ -37,7 +38,7 @@ public class OrderServiceImpl implements OrderService {
 //    롬복 사용 위해 생성자 지움
     @Autowired
 //    public OrderServiceImpl(MemberRepository memberRepository, @Qualifier("mainDiscountPolicy") DiscountPolicy discountPolicy) {
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
