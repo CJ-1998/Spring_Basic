03d1237d09b4229dea685df5cb687c73e0d87abd
CJ <choijun0627@konkuk.ac.kr>
2023-07-11 08:35:25 +0900

    <refactor> 스프링 핵심 원리 기본편 프로젝트 section 9
    Provider를 사용해 오류 해결
    
    이전 코드는 request 오기 전에 의존 관계 주입하려해 오류 난다.
    Provider 사용해서 request 있을 때까지 요청을 지연한 것이다.
    Provider 사용하는 것으로 controller, service 변경

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -3,6 +3,7 @@ package hello.core.web;
 import hello.core.common.MyLogger;
 import hello.core.web.LogDemoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,11 +13,12 @@ import javax.servlet.http.HttpServletRequest;
 @RequiredArgsConstructor
 public class LogDemoController {
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
         String requestURL = request.getRequestURL().toString();
+        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestURL(requestURL);
         myLogger.log("controller test");
         logDemoService.logic("testId");
