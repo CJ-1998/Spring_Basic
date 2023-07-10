4704278c920b4c8e8a54b569ed9a71a784e69100
CJ <choijun0627@konkuk.ac.kr>
2023-07-10 16:52:00 +0900

    <refactor> 스프링 핵심 원리 기본편 프로젝트 section 8
    인터페이스로 초기화와 소멸 전 콜백 받는 방법 확인
    
    InitializingBean, DisposableBean 사용해서 초기화와 소멸 전 콜백 받는
    것을 적용하도록 NetworkClient 클래스 수정

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,28 +1,34 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
     private String url;
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
     }
-
     public void setUrl(String url) {
         this.url = url;
     }
-
     //서비스 시작시 호출
     public void connect() {
         System.out.println("connect: " + url);
     }
-
     public void call(String message) {
         System.out.println("call: " + url + " message = " + message);
     }
-
     //서비스 종료시 호출
-    public void disconnect() {
-        System.out.println("close: " + url);
+    public void disConnect() {
+        System.out.println("close + " + url);
+    }
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        connect();
+        call("초기화 연결 메시지");
+    }
+    @Override
+    public void destroy() throws Exception {
+        disConnect();
     }
 }
